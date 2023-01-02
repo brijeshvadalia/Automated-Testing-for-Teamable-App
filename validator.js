fc0ed8d78ea0843d01adfc905ef18947972d646f
Brijesh Vadaliya <76428127+brijeshvadalia@users.noreply.github.com>
2023-01-02 05:38:03 +0530

    Modified

--- validator.js
+++ validator.js
@@ -2,9 +2,11 @@ function isInvalidEmail(userObject) {
     return !userObject.email.includes("@")
 }
 
+
 function isEmptyPayload(userObject) {
     return Object.keys(userObject).length === 0
 }
+
 
 module.exports = {
     isInvalidEmail,
